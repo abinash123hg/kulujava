541338309432fcd87063aad57b97d093b9b51174
abinash123hg <swainabinash839@gmail.com>
2025-07-20 19:56:08 +0530

    Add files via upload
    question java

--- unction.java
+++ unction.java
@@ -1,24 +1,156 @@
+// 1.	Write a Java program that accepts two integers and prints their sum, difference, product, quotient, and remainder.
+// 2.	Implement a program that checks if a number is divisible by both 3 and 5 using logical AND (&&).
+// 3.	Write a program to show the result of bitwise AND, OR, XOR, and NOT on two given integers.
+// 4.	Write a program that accepts a student's mark and prints “Pass” if it’s 40 or above, otherwise “Fail.”
+// 5.	Accept a number and check if it's positive, negative, or zero using nested if-else.
+// 6.	Create a calculator using switch that handles basic operations (+, -, *, /) based on user input.
+// 7.	Write a Java program that prints the multiplication table of a given number from 1 to 10.
+// 8.	WAP calculate the factorial of a given positive integer.
+// 9.	Simulate a simple login system where the user gets repeated chances to enter the correct password using do-while.
+// 10.	Develop a program to print numbers from 1 to 20, skipping all multiples of 3 using continue.
 import java.util.*;
 
 public class unction {
-    public static long Power(long x, long n) {
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result = result * x;
-        }
-        return result;
-    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your x or base: ");
-        long x = sc.nextLong();
-        System.out.print("Enter your n or exponent: ");
-        long n = sc.nextLong();
+        // // 1.	Write a Java program that accepts two integers and prints their sum, difference, product, quotient, and remainder.
 
-        long result = Power(x, n);
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your x or base: ");
+        // long x = sc.nextLong();
+        // System.out.print("Enter your n or exponent: ");
+        // long n = sc.nextLong();
 
-        System.out.println("Result is: " + result);
-    }
+        // long result = Power(x, n);
+        // sc.close();
+
+        // System.out.println("Result is: " + result);
+        // Scanner sc  = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println("sum of " + a + "and " + b  + "is" + (a+b));
+        // System.out.println("difference of "  + a +"and "+b + "is" + (a-b));
+        // System.out.println("multiplication of "  +  a  +"and "+ b  + "is" + (a*b));
+        // System.out.println("quotient of "  +  a  +"and "+b + "is" + (a/b));
+        // System.out.println("reminder of "  +  a  +"and "+b + "is" + (a%b));
+        // System.out.println("thank u");
+        // sc.close();
+                    //    
+// 2.	Implement a program that checks if a number is divisible by both 3 and 5 using logical AND (&&).
+
+        //    Scanner sc = new Scanner(System.in);
+        //    System.out.println("enter ur number\n");
+        //    int num = sc.nextInt();
+        //    if (num %3 ==0 && num%5==0) {
+        //     System.out.println("ur number is divisible by both 3 and 5\n");
+        //    }
+        //    else{
+        //     System.out.println("ur numbe is not divisible by both 3 and 5");
+        //    }
+        //    sc.close();  
+        
+// 3.	Write a program to show the result of bitwise AND, OR, XOR, and NOT on two given integers.
+            // Scanner sc = new Scanner(System.in);
+            // System.out.println("enter ur number a\n");
+            // int a = sc.nextInt();
+            // System.out.println("enter ur second number  b:");
+            // int b = sc.nextInt();
+            // System.out.println("result of bitwise AND is:"+(a & b));
+            
+            // System.out.println("result of bitwise OR is:"+(a | b ));
+            
+            // System.out.println("result of bitwise XOR is:"+(a ^ b));
+            
+            // System.out.println("result of bitwise NOT of A is:"+( ~ a));
+            // System.out.println("result of bitwise NOT of b is:"+(~ b));
+            // System.out.println("Thank u");
+            
+// 4.	Write a program that accepts a student's mark and prints “Pass” if it’s 40 or above, otherwise “Fail.”
+            //   Scanner sc = new Scanner(System.in);
+            //   System.out.println("Enter the marks of the students: ");
+            //   int marks = sc.nextInt();
+            //   if (marks>=40) {
+            //     System.out.println("u pass\n");
+            //   }
+            //   else{
+            //     System.out.println("fail\n");
+            //   }
+// Accept a number and check if it's positive, negative, or zero using nested if-else.
+                //   Scanner sc  = new Scanner(System.in);
+                //   System.out.println("enter ur number: ");
+                //   int num = sc.nextInt();
+                //    if (num>0) {
+                //     System.out.println("ur number is positive\n");
+                //    }
+                //    else if (num<0) {
+                //     System.out.println("ur number is negative\n");
+                //    }
+                //    else if (num ==0) {
+                //     System.out.println("ur num is zero\n");
+                //    }
+                //    System.out.println("thanku\n");
+                   
+// 6.	Create a calculator using switch that handles basic operations (+, -, *, /) based on user input.
+                // Scanner sc = new Scanner(System.in);
+                // System.out.println("enter ur 1st number");
+                // int a = sc.nextInt();
+                // System.out.println("enter ur 2nd number\n");
+                // int b = sc.nextInt();
+                // System.out.println("enter the operation u want like (+ for 1,- for 2,* for 3,% for 4,/ for 5)");
+                // int num = sc.nextInt();
+
+                // switch (num) {
+                //     case 1:System.out.println("sum is:"+a+b);              
+                //         break;
+                //     case 2:System.out.println("diff is:"+ (a-b));              
+                //         break;
+                //         case 3:System.out.println("product is is:"+a*b);              
+                //         break;
+                //         case 4:System.out.println("reminder  is:"+a%b);              
+                //         break;
+                //         case 5:System.out.println("division is:"+a/b);              
+                //         break;
+                            
+                //     default:System.out.println("pls check again what u click");
+                //         break;
+                // }
+                // sc.close();
+                // System.out.println("thank u ");
+    // 7.	Write a Java program that prints the multiplication table of a given number from 1 to 10.
+                // Scanner sc = new Scanner(System.in);
+                // int num = sc.nextInt();
+                // for(int i = 1;i<=10;i++){
+                //     System.out.println(num * i);
+                // }
+                // System.out.println("thank u");
+    
+// 8.	WAP calculate the factorial of a given positive integer.
+                // Scanner sc = new Scanner(System.in);
+                // int num = sc.nextInt();
+                // int factorial = 1;
+                // for(int i = 1;i<=num;i++){
+                //       factorial = factorial*i;
+                // }
+                // System.out.println("factorial is " + factorial);
+                // System.out.println("thanku");
+     
+// 9.	Simulate a simple login system where the user gets repeated chances to enter the correct password using do-while.
+            //     Scanner sc = new Scanner(System.in);
+            //   int correctpassword = 3223;
+              
+            //     System.out.println("enter ur correct password: ");
+            //   do{
+            //     int password = sc.nextInt();
+            //     if (password==correctpassword) {
+            //         System.out.println("Acess granted");
+            //         break;
+            //     }
+            //     else{
+            //         System.out.println("try again");
+            //     }
+
+            //   }while(true);
+            //   System.out.println("thank u");
+          }
+
 }
